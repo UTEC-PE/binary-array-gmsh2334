e04ba4ceb425cf4319d7961cb4543d2559312dba
gonzalo <gonzalo.munoz@utec.edu.pe>
2018-10-30 23:39:11 -0500

    finished

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include "boolean.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl;
-    cout << "\Binary Array Practice" << endl;
+    cout << "Binary Array Practice" << endl;
     cout << "===========================================================" << endl << endl;
+
+    int tmn=8;
+    boolean bl(tmn);
+    for(int i=0; i <tmn ; i++){
+        bl.bitOn(i);
+    }
+    bl.bitOff(3);
+    for(int i=tmn-1; i!=-1 ; i--){
+    bl.operator[](i);
+    }
+    bl.clear();
+    cout << "\n\n";
+    for(int i=tmn-1; i!=-1 ; i--){
+        bl.operator[](i);
+    }
+    bl.~boolean();
 
     system("read");
     return EXIT_SUCCESS;
